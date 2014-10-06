b35f7637e7c5155f8e8fce01d389109c10efe523
Mohammad Alizadeh <moattar@cisco.com>
2014-10-06 15:26:16 -0700

    Cleanup of server code. Added error handling.

--- src/server/server.c
+++ src/server/server.c
@@ -1,14 +1,18 @@
-#include "server.h"
-#include <fcntl.h>
-#include "sys/sendfile.h"
-#include <sys/stat.h>
-#include <sys/time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/socket.h>
 #include <netinet/tcp.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "server.h"
 
 // global variables
 int serverPort;
 int listenfd;
-char filebuf[MAX_WRITE];
+char flowbuf[MAX_WRITE];
 
 int main (int argc, char *argv[]) {
   socklen_t len;
@@ -21,28 +25,26 @@ int main (int argc, char *argv[]) {
   
   // initialize socket
   listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (listenfd < 0)
+    error("ERROR opening socket");
 
-  setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &sock_opt, sizeof(sock_opt));
-  setsockopt(listenfd, IPPROTO_TCP, TCP_NODELAY, &sock_opt, sizeof(sock_opt));
+  if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &sock_opt, sizeof(sock_opt)) < 0)
+    error("ERROR setting SO_REUSERADDR option");
+  if (setsockopt(listenfd, IPPROTO_TCP, TCP_NODELAY, &sock_opt, sizeof(sock_opt)) < 0)
+    error("ERROR setting TCP_NODELAY option");
 
   memset(&servaddr, 0, sizeof(servaddr));
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
   servaddr.sin_port = htons(serverPort);
   
-  int ret = bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
-  if (ret < 0) {
-    fprintf(stderr, "error in bind: %s\n", strerror(errno));
-    exit(-1);
-  }
+  if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    error("ERROR on bind");
   
-  ret = listen(listenfd, 20);
-  if (ret < 0) {
-    fprintf(stderr,"error in listen: %s\n", strerror(errno));
-    exit(-1);
-  }
+  if (listen(listenfd, 20) < 0)
+    error("ERROR on listen");
   
-  printf("TCP test application server started...\n");
+  printf("Dynamic traffic generator application server started...\n");
   printf("Listening port: %d\n", serverPort);
 
   while(1) {
@@ -50,84 +52,106 @@ int main (int argc, char *argv[]) {
     int sockfd;
     len = sizeof(cliaddr);
     sockfd = accept(listenfd, (struct sockaddr *) &cliaddr, &len);
-    
+    if (sockfd < 0)
+      error("ERROR on accept");
+
     pid_t pid = fork();
-    if (pid < 0) {
-      fprintf(stderr, "fork failed... exiting\n");
-      exit(-1);
-    }
+    if (pid < 0)
+      error("ERROR on fork");
 
     // child process
     if (pid == 0) {
-      close(listenfd);
-      getDataFromTheClient(sockfd, (const struct sockaddr_in *) &cliaddr);
+      if (close(listenfd) < 0)
+	error("child: ERROR on close");
+      handle_connection(sockfd, (const struct sockaddr_in *) &cliaddr);
       break;
     }
     // parent process
     else {
-      close(sockfd);
+      if (close(sockfd) < 0)
+	error("parent: ERROR on close");
     }
   }
 
   return 0;
 }
 
-// listen client
-void getDataFromTheClient(int sockfd, const struct sockaddr_in *cliaddr) {
+/* 
+ * Handles requests for an established connection. Each request is initiated
+ * by the client with a small message providing meta-data for the request, 
+ * specifically, a flow index and size. The server echoes the meta-data, and
+ * subsequently sends a flow of the requested size to the client.
+ */ 
+void handle_connection(int sockfd, const struct sockaddr_in *cliaddr) {
   uint f_index;
   uint f_size;
   char buf[50];
+  int meta_data_size = 2 * sizeof(uint);
   char clistr[INET_ADDRSTRLEN];
-  inet_ntop(AF_INET, &(cliaddr->sin_addr), clistr, INET_ADDRSTRLEN);
+
+  if (inet_ntop(AF_INET, &(cliaddr->sin_addr), clistr, INET_ADDRSTRLEN) == NULL)
+    error("ERROR on inet_ntop");
 
   printf("Connection established to %s (sockfd = %d)!\n", clistr, sockfd);
 
   while(1) {
-    // read request
-    int n = read(sockfd, buf, 2 * sizeof(uint));
-    if (n <= 0) {
-      if (n < 0)
-	perror("error in meta-data read");
-      break;
-    } 
-    
+    // read meta-data
+    int total = 0;
+    do {
+      int n = read(sockfd, buf + total, meta_data_size - total);
+      if (n <= 0) {
+	if (n < 0) 
+	  perror("ERROR in meta-data read");
+	goto close_connection;
+      }
+      total += n;
+    } while (total < meta_data_size);
+                   
     memcpy(&f_index, buf, sizeof(uint));
     memcpy(&f_size, buf + sizeof(uint), sizeof(uint));
 
 #ifdef DEBUG
-    printf("File request: index: %u size: %d\n", f_index, f_size);
+    printf("Flow request: index: %u size: %d\n", f_index, f_size);
 #endif 
     
-    // send meta data (f_index and f_size)
-    n = write(sockfd, buf, 2 * sizeof(uint));
-    if (n < 0) {
-      perror("error in meta-data write");
-      break;
-    }
-
-    // send file
-    uint total = f_size;
+    // echo meta-data (f_index and f_size)
+    total = 0;
+    do {
+      int n = write(sockfd, buf + total, meta_data_size - total);
+      if (n < 0) {
+	perror("error in meta-data write");
+	goto close_connection;
+      }
+      total += n;
+    } while (total < meta_data_size);
+    
+    // send flow of f_size bytes
+    total = f_size;
     do {
       uint bytes_to_send;
       if (total > MAX_WRITE)
 	bytes_to_send = MAX_WRITE;
       else
 	bytes_to_send = total;
-      int bytes_sent = write(sockfd, filebuf, bytes_to_send);
       
+      int bytes_sent = write(sockfd, flowbuf, bytes_to_send);  
       if (bytes_sent < 0) {
 	perror("error in file write");
-	break;
+	goto close_connection;
       }
       
       total -= bytes_sent;
     } while (total > 0); 
   }
 
-  printf("Connection to %s closed (sockfd = %d)!\n", clistr, sockfd);
+ close_connection:
   close(sockfd);
+  printf("Connection to %s closed (sockfd = %d)!\n", clistr, sockfd);
 }
 
+/*
+ * Read command line arguments. 
+ */
 void read_args(int argc, char*argv[]) {
   // default values
   serverPort = 5000;
@@ -142,7 +166,13 @@ void read_args(int argc, char*argv[]) {
       printf("usage: server [options]\n");
       printf("options:\n");
       printf("-p <value>                 port number (default 5000)\n");
-      exit(1);
+      exit(EXIT_FAILURE);
     }
   }
+}
+
+void error(const char *msg)
+{
+  perror(msg);
+  exit(EXIT_FAILURE);
 }
--- src/server/server.h
+++ src/server/server.h
@@ -1,30 +1,14 @@
 #ifndef __server_h
 #define __server_h
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <assert.h>
-#include <pthread.h>
-#include <signal.h>
-#include <ctype.h>
-#include <errno.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-
 #define MAX_WRITE 104857600  // 100MB
 
 //#define DEBUG
 
 // prototypes                                                
-void getDataFromTheClient(int sockfd, const struct sockaddr_in *cliaddr);
+void handle_connection(int sockfd, const struct sockaddr_in *cliaddr);
 void read_args(int argc, char*argv[]);
-
-
+void error(const char *msg);
 
 #endif
 
